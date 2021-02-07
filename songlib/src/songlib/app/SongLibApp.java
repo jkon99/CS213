64d9ca84a01390a91f143cbdf05073fcb3e3d2ed
timtopia <35663613+timtopia@users.noreply.github.com>
2021-02-07 17:21:28 -0500

    Persistant File

--- songlib/src/songlib/app/SongLibApp.java
+++ songlib/src/songlib/app/SongLibApp.java
@@ -1,5 +1,9 @@
 package songlib.app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,6 +15,20 @@ public class SongLibApp extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
+		File list = new File("songLib.txt");
+		try {
+			if(!list.createNewFile())
+				throw new FileNotFoundException("File already Exist");
+		} catch (FileNotFoundException e) {
+		}
+		Scanner lineReader = new Scanner(list);
+		while(lineReader.hasNextLine()) {
+			String line = lineReader.nextLine();
+			
+		}
+		
+		
+		
 		
 		VBox root = (VBox)loader.load();
 		Scene scene = new Scene(root);
@@ -18,7 +36,6 @@ public class SongLibApp extends Application {
 		primaryStage.setTitle("Song Library");
 		primaryStage.setResizable(false);
 		primaryStage.show();
-		
 	}
 	
 	public static void main(String[] args) {
